b3f322c0a85933d2dea186fa3e53f2067db10a35
Daniel Dario Fula Arguello <ddfulaa@unal.edu.co>
2019-10-11 16:20:30 -0500

    Se sube el primer ejemplo y un archivo léeme que indica cómo usar la aplicación APIO para crear proyectos.

---
+++ Ejemplo1/ejemplo1.v
@@ -0,0 +1,14 @@
+# Encender todos los leds de la tarjeta
+module ejemplo1(output wire BOARD_LED0,
+            output wire BOARD_LED1,
+            output wire BOARD_LED2,
+            output wire BOARD_LED3,
+            output wire BOARD_LED4);
+
+assign BOARD_LED0 = 1'b0;
+assign BOARD_LED1 = 1'b1;
+assign BOARD_LED2 = 1'b1;
+assign BOARD_LED3 = 1'b1;
+assign BOARD_LED4 = 1'b1;
+
+endmodule
