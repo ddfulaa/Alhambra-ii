38d757e88cda798e20e5cd0555844e9812eeb91e
Daniel Dario Fula Arguello <ddfulaa@unal.edu.co>
2019-10-12 09:14:08 -0500

    Corrección 2 del clock de 1Hz

--- clock_divider_1Hz/clock_divider_1Hz.v
+++ clock_divider_1Hz/clock_divider_1Hz.v
@@ -1,9 +1,17 @@
 // Hacer un clock de 1Hz
 module clock_divider_1Hz(input BOARD_CLK,
-            output reg BOARD_LED0);
-
+            output reg BOARD_LED0,
+			output reg BOARD_LED1,
+			output reg BOARD_LED2,
+			output reg BOARD_LED3,
+			output reg BOARD_LED4,
+			output reg BOARD_LED5,
+			output reg BOARD_LED6,
+			output reg BOARD_LED7);
+	
     reg [23:0] divcounter= 24'b0;
     wire clk_out;
+	
     always @(posedge BOARD_CLK)
         begin
             divcounter <= divcounter + 1; 
@@ -12,5 +20,14 @@ module clock_divider_1Hz(input BOARD_CLK,
         end
     assign clk_out = divcounter[23];
     always @(posedge clk_out)
+		begin
         BOARD_LED0 = ~BOARD_LED0;
+		BOARD_LED1 = BOARD_LED2;
+		BOARD_LED2 = ~BOARD_LED2;
+		BOARD_LED3 = 1'b0;
+		BOARD_LED4 = 1'b0;
+		BOARD_LED5 = 1'b0;
+		BOARD_LED6 = 1'b0;
+		BOARD_LED7 = 0;
+		end
 endmodule
